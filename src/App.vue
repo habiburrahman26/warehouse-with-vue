ec1bf3d53e0e30c693685dcbefec0daea3b6e9f0
HabiburRahmanAnik <habib264521@gmail.com>
2022-08-25 21:54:35 +0600

    header

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <Header/>
+  <div></div>
+</template>
+
+<script>
+import Header from './Header/Header.vue';
+export default {
+  components: {
+    Header,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
---
+++ src/Header/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <header class="px-28 py-3">
+    <nav class="flex items-center gap-14">
+      <p class="text-3xl font-semibold">E-Store</p>
+      <ul class="flex items-center gap-5 text-lg">
+        <li>
+          <router-link class="font-medium" to="/home">Home</router-link>
+        </li>
+        <li>
+          <router-link class="font-medium" to="/manageItem"
+            >Manage Items</router-link
+          >
+        </li>
+        <li>
+          <router-link class="font-medium" to="/addItem">Add Item</router-link>
+        </li>
+        <li>
+          <router-link class="font-medium" to="/myItem">My Item</router-link>
+        </li>
+      </ul>
+      <div class="ml-auto">
+        <button class="px-4 py-2 font-medium border border-transparent hover:border hover:border-pink-400 rounded mr-4">Login</button>
+        <button class="px-4 py-2 bg-blue-500 hover:bg-blue-600 rounded text-white font-semibold">
+          SignUp
+        </button>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
